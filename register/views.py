b93ab91bf2dd22fc3ef80ea48c812452b82c2478
Parth Singh <parthsingh287@gmail.com>
2019-10-07 04:02:31 +0530

    Default votes bug fixed

--- register/views.py
+++ register/views.py
@@ -21,7 +21,9 @@ def candidate(request):
 	if request.method=='POST':
 		form = candidateForm(request.POST)
 		if form.is_valid():
-		    form.save(commit=True)
+		    can=form.save(commit=False)
+		    can.votes=0
+		    can.save()
 		return redirect('/register/candidate')
 	else:
 		form = candidateForm()
